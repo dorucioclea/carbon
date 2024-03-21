294064c8589f83530ea07ad2ee8da69b5e673b5c
crudmachine <162540227+crudmachine@users.noreply.github.com>
2024-03-20 22:55:30 -0400

    useCustomFieldsSchema

--- apps/carbon/app/hooks/useCustomColumns.tsx
+++ apps/carbon/app/hooks/useCustomColumns.tsx
@@ -1,54 +1,43 @@
-import type { Json } from "@carbon/database";
-import { useMemo } from "react";
 import type { ColumnDef } from "@tanstack/react-table";
-import { z } from "zod";
 import { DataType } from "~/modules/shared";
-import { path } from "~/utils/path";
-import { useRouteData } from "./useRouteData";
-//import UserSelect from "~/components/Selectors/UserSelect";
+import { useCustomFieldsSchema } from "./useCustomFieldsSchema";
 
 export function useCustomColumns(table: string) {
-  const data = useRouteData<{
-    customFields: { table: string; name: string; fields: Json[] }[];
-  }>(path.to.authenticatedRoot);
-
-  const schema = useMemo<Fields>(() => {
-    let schemaResult: Fields = [];
-    data?.customFields.forEach((field) => {
-      if (field.table === table) {
-        const fields = fieldValidator.safeParse(field.fields);
-        if (fields.success && "table" in field) {
-          schemaResult = fields.data;
-        }
-      }
-    });
-    return schemaResult;
-  }, [data?.customFields, table]);
-
-  if (!schema) {
-    return [];
-  }
+  const customFieldsSchemas = useCustomFieldsSchema();
+  const schema = customFieldsSchemas?.[table];
 
   const customColumns: ColumnDef<any>[] = [];
 
-  schema.forEach((field) => {
+  schema?.forEach((field) => {
     customColumns.push({
       accessorKey: `customFields.${field.id}`,
       header: field.name,
       cell: (item) => {
         switch (field.dataTypeId) {
           case DataType.Boolean:
-            return item.row.original?.customFields ? (item.row.original?.customFields[field.id] === "on" ? "Yes" : "No") : null;
+            return item.row.original?.customFields
+              ? item.row.original?.customFields[field.id] === "on"
+                ? "Yes"
+                : "No"
+              : null;
           case DataType.Date:
-            return item.row.original?.customFields ? item.row.original?.customFields[field.id] : null;
+            return item.row.original?.customFields
+              ? item.row.original?.customFields[field.id]
+              : null;
           case DataType.List:
-            return item.row.original?.customFields ? item.row.original?.customFields[field.id] : null;
+            return item.row.original?.customFields
+              ? item.row.original?.customFields[field.id]
+              : null;
           case DataType.Numeric:
-            return item.row.original?.customFields ? item.row.original?.customFields[field.id] : null;
+            return item.row.original?.customFields
+              ? item.row.original?.customFields[field.id]
+              : null;
           case DataType.Text:
-            return item.row.original?.customFields ? item.row.original?.customFields[field.id] : null;
+            return item.row.original?.customFields
+              ? item.row.original?.customFields[field.id]
+              : null;
           case DataType.User:
-            return null /*<UserSelect
+            return null; /*<UserSelect
                     type="employee"
                     usersOnly
                     isMulti={false}
@@ -58,21 +47,9 @@ export function useCustomColumns(table: string) {
           default:
             return null;
         }
-      }});
+      },
+    });
   });
 
   return customColumns;
 }
-
-const fieldValidator = z
-  .array(
-    z.object({
-      dataTypeId: z.number(),
-      id: z.string(),
-      listOptions: z.array(z.string()).nullable(),
-      name: z.string(),
-      sortOrder: z.number(),
-    })
-  )
-  .nullable();
-type Fields = z.infer<typeof fieldValidator>;
