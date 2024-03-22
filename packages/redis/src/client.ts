ecb1da77812a4ead3eb8825cbd8be396bd08cdf1
Brad Barbin <bradbarbin@protonmail.com>
2024-03-22 13:45:05 -0600

    refactor: rename enviornment variables to match convention

--- packages/redis/src/client.ts
+++ packages/redis/src/client.ts
@@ -1,20 +1,20 @@
 import type { Redis as RedisType } from "@upstash/redis";
 import { Redis } from "@upstash/redis";
 
-const UPSTASH_REDIS_URL = process.env.UPSTASH_REDIS_URL;
-const UPSTASH_REDIS_TOKEN = process.env.UPSTASH_REDIS_TOKEN;
+const UPSTASH_REDIS_REST_URL = process.env.UPSTASH_REDIS_REST_URL;
+const UPSTASH_REDIS_REST_TOKEN = process.env.UPSTASH_REDIS_REST_TOKEN;
 let redis: RedisType;
 
 declare global {
   var __redis: RedisType | undefined;
 }
 
-if (!UPSTASH_REDIS_URL) {
-  throw new Error("UPSTASH_REDIS_URL is not defined");
+if (!UPSTASH_REDIS_REST_URL) {
+  throw new Error("UPSTASH_REDIS_REST_URL is not defined");
 }
 
-if (!UPSTASH_REDIS_TOKEN) {
-  throw new Error("UPSTASH_REDIS_TOKEN is not defined");
+if (!UPSTASH_REDIS_REST_TOKEN) {
+  throw new Error("UPSTASH_REDIS_REST_TOKEN is not defined");
 }
 
 // this is needed because in development we don't want to restart
@@ -22,14 +22,14 @@ if (!UPSTASH_REDIS_TOKEN) {
 // create a new connection to the Redis with every change either.
 if (process.env.NODE_ENV === "production") {
   redis = new Redis({
-    url: UPSTASH_REDIS_URL,
-    token: UPSTASH_REDIS_TOKEN,
+    url: UPSTASH_REDIS_REST_URL,
+    token: UPSTASH_REDIS_REST_TOKEN,
   });
 } else {
   if (!global.__redis) {
     global.__redis = new Redis({
-      url: UPSTASH_REDIS_URL,
-      token: UPSTASH_REDIS_TOKEN,
+      url: UPSTASH_REDIS_REST_URL,
+      token: UPSTASH_REDIS_REST_TOKEN,
     });
   }
   redis = global.__redis;
