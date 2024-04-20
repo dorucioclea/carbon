3298ea2113e35e9c7179cdd41d64d0ea29d13684
neilk17 <kanakianeil@gmail.com>
2024-04-20 15:07:00 -0400

    update copy for story

--- apps/website/src/app/story/page.tsx
+++ apps/website/src/app/story/page.tsx
@@ -8,22 +8,36 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <div className="prose dark:prose-invert container max-w-[750px] my-24 text-xl space-y-8">
+    <div className="prose dark:prose-invert container max-w-xl my-24 text-xl space-y-8">
       <h1 className="text-3xl font-semibold sm:text-2xl lg:text-4xl xl:text-5xl text-center">
         Building Carbon
       </h1>
       <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book.
+        In the year 2004, a state of the art manufacturing ERP tried to be a
+        one-stop-shop for manufacturers&apos; software needs: estimating,
+        scheduling, inventory, accounting, and more, all in a single package. It
+        is now 2024, and manufacturing ERPs look remarkably similar to how they
+        did 20 years ago. There are a handful of companies dedicated to
+        continuous improvement and innovation, but the majority are resting on
+        their laurels.
+      </p>
+
+      <p>
+        The truth of the matter is this: the world has changed a lot in the last
+        20 years, and the cutting-edge software of today would be nearly
+        unrecognizable to our past selves. Pioneers in the software industry
+        have shown us what is possible, and we as software users have come to
+        expect more from our tools.
       </p>
       <p>
-        It has survived not only five centuries, but also the leap into
-        electronic typesetting, remaining essentially unchanged. It was
-        popularized in the 1960s with the release of Letraset sheets containing
-        Lorem Ipsum passages, and more recently with desktop publishing software
-        like Aldus PageMaker including versions of Lorem Ipsum.
+        We now live in a connected world. A single software tool can no longer
+        be everything to everyone. In our daily lives we interact with an
+        ecosystem of connected software - why should it be any different for a
+        manufacturing business? The mission of Carbon ERP is to become the
+        platform upon which connected manufacturing software ecosystems are
+        built. The goal is not to do everything; the goal is to be the glue that
+        connects the most innovative tools that the leading minds in
+        manufacturing technology can dream up.
       </p>
       <BsHexagonFill />
     </div>
