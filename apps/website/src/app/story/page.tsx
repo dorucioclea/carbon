f9647c0e19509d06a02e9bfbba0b3758ed7f51dc
Brad Barbin <bradbarbin@protonmail.com>
2024-04-21 20:58:51 -0400

    docs: condense copy on story

--- apps/website/src/app/story/page.tsx
+++ apps/website/src/app/story/page.tsx
@@ -1,9 +1,8 @@
 /* eslint-disable react/no-unescaped-entities */
 import type { Metadata } from "next";
-import { BsHexagonFill } from "react-icons/bs";
 
 export const metadata: Metadata = {
-  title: "Carbon ERP | Story",
+  title: "Carbon ERP | Why",
 };
 
 export default function Page() {
@@ -12,34 +11,44 @@ export default function Page() {
       <h1 className="text-3xl font-semibold sm:text-2xl lg:text-4xl xl:text-5xl text-center">
         Building Carbon
       </h1>
-      <p>
-        In the year 2004, a state of the art manufacturing ERP tried to be a
-        one-stop-shop for manufacturers&apos; software needs: estimating,
-        scheduling, inventory, accounting, and more, all in a single package. It
-        is now 2024, and manufacturing ERPs look remarkably similar to how they
-        did 20 years ago. There are a handful of companies dedicated to
-        continuous improvement and innovation, but the majority are resting on
-        their laurels.
-      </p>
 
       <p>
-        The truth of the matter is this: the world has changed a lot in the last
-        20 years, and the cutting-edge software of today would be nearly
-        unrecognizable to our past selves. Pioneers in the software industry
-        have shown us what is possible, and we as software users have come to
-        expect more from our tools.
+        Like a lot of people, we've been through our share of bad ERP
+        implementations. And like a lot of people, we've come to expect a lot
+        from our software.
       </p>
+
+      <h2 className="text-xl font-semibold sm:text-lg lg:text-2xl xl:text-3xl">
+        Open Source
+      </h2>
       <p>
-        We now live in a connected world. A single software tool can no longer
-        be everything to everyone. In our daily lives we interact with an
-        ecosystem of connected software - why should it be any different for a
-        manufacturing business? The mission of Carbon ERP is to become the
-        platform upon which connected manufacturing software ecosystems are
-        built. The goal is not to do everything; the goal is to be the glue that
-        connects the most innovative tools that the leading minds in
-        manufacturing technology can dream up.
+        We've built Carbon for our past selves. And we've open-sourced it
+        because we know we're not alone. Together, we are a community of
+        manufacturers and developers who see a better way. And we understand
+        that in order to make a system that is customized to your business,
+        you've got to own all the code.
       </p>
-      <BsHexagonFill />
+
+      <h2 className="text-xl font-semibold sm:text-lg lg:text-2xl xl:text-3xl">
+        Manufacturing-Focused
+      </h2>
+      <p>
+        Manufacturing is hard enough. It's even harder when you're trying to
+        make your processes match the ERP's expectations. We've built Carbon to
+        be flexible, and we've built it to be simple so that you can model your
+        business as closely as possible.
+      </p>
+
+      <h2 className="text-xl font-semibold sm:text-lg lg:text-2xl xl:text-3xl">
+        Extensible
+      </h2>
+      <p>
+        The mission of Carbon ERP is to become the platform upon which connected
+        manufacturing ecosystems are built. The goal is to provide a solid
+        foundation for businesses to build upon, and to provide a platform that
+        is flexible enough to adapt to the ever-changing needs of the
+        manufacturing industry.
+      </p>
     </div>
   );
 }
