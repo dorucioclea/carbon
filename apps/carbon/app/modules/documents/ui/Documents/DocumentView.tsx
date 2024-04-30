a373c0cf81a31168aa3b5de6e5426f69c3738412
neilk17 <kanakianeil@gmail.com>
2024-04-30 13:43:44 -0400

    use react-pdf instead of iframe

--- apps/carbon/app/modules/documents/ui/Documents/DocumentView.tsx
+++ apps/carbon/app/modules/documents/ui/Documents/DocumentView.tsx
@@ -1,7 +1,11 @@
 import { Button, ResizableHandle, ResizablePanel } from "@carbon/react";
 import { useNavigate } from "@remix-run/react";
+import { Suspense, useState } from "react";
 import { LuX } from "react-icons/lu";
+import { Document, Page, pdfjs } from "react-pdf";
 import { path } from "~/utils/path";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
 type DocumentPreviewProps = {
   ownerId: string;
@@ -14,6 +18,12 @@ const DocumentView = ({
   bucket,
   pathToFile,
 }: DocumentPreviewProps) => {
+  const [numPages, setNumPages] = useState<number>();
+
+  function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
+    setNumPages(numPages);
+  }
+
   const navigate = useNavigate();
   const onClose = () => navigate(path.to.documents);
 
@@ -24,13 +34,24 @@ const DocumentView = ({
         <Button isIcon variant={"ghost"} onClick={onClose}>
           <LuX className="w-4 h-4" />
         </Button>
-        <iframe
-          seamless
-          title={pathToFile}
-          width="100%"
-          height="100%"
-          src={path.to.file.previewFile(`${bucket}/${pathToFile}`)}
-        />
+        <Suspense>
+          <Document
+            file={path.to.file.previewFile(`${bucket}/${pathToFile}`)}
+            onLoadSuccess={onDocumentLoadSuccess}
+            className="h-full "
+          >
+            <div className="overflow-auto" style={{ height: "100vh" }}>
+              {Array.from(new Array(numPages), (_, index) => (
+                <Page
+                  key={`page_${index + 1}`}
+                  pageNumber={index + 1}
+                  renderTextLayer={false}
+                  width={680}
+                />
+              ))}
+            </div>
+          </Document>
+        </Suspense>
       </ResizablePanel>
     </>
   );
