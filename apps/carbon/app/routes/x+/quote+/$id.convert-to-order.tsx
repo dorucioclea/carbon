54d24e89de16cee3218af78d729025f63cce17a3
crudmachine <162540227+crudmachine@users.noreply.github.com>
2024-05-04 14:28:41 +0530

    use requestReferrer

--- apps/carbon/app/routes/x+/quote+/$id.convert-to-order.tsx
+++ apps/carbon/app/routes/x+/quote+/$id.convert-to-order.tsx
@@ -1,18 +1,9 @@
 import { redirect, type ActionFunctionArgs } from "@remix-run/node";
-import type { SalesOrderLine } from "~/modules/sales";
 import { createSalesOrderFromQuote } from "~/modules/sales/sales.server";
-import {
-  getQuote,
-  getQuoteLines,
-  convertQuoteToOrder,
-  upsertSalesOrder,
-  insertSalesOrderLines,
-} from "~/modules/sales";
-import { getNextSequence, rollbackNextSequence } from "~/modules/settings";
 import { requirePermissions } from "~/services/auth/auth.server";
 import { flash } from "~/services/session.server";
 import { assertIsPost } from "~/utils/http";
-import { path } from "~/utils/path";
+import { path, requestReferrer } from "~/utils/path";
 import { error, success } from "~/utils/result";
 
 export async function action(args: ActionFunctionArgs) {
@@ -32,7 +23,7 @@ export async function action(args: ActionFunctionArgs) {
     const result = await createSalesOrderFromQuote(id, userId);
     if (result.error || !result?.data) {
       throw redirect(
-        request.headers.get("Referer") ?? path.to.quotes,
+        requestReferrer(request) ?? path.to.quote(id),
         await flash(
           request,
           error(result.error, "Failed to create sales order")
