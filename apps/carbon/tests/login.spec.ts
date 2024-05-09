030d7cba539a244c45757fe5989cc47c6300512c
neilk17 <kanakianeil@gmail.com>
2024-05-09 14:16:35 -0400

    playwright login test

--- apps/carbon/tests/login.spec.ts
+++ apps/carbon/tests/login.spec.ts
@@ -3,4 +3,10 @@ import { test, expect } from "@playwright/test";
 test("login", async ({ page }) => {
   await page.goto("http://localhost:3000/");
   await expect(page).toHaveTitle("Carbon | Login");
+  await page.click("id=email");
+  await page.locator("id=email").fill("admin@carbon.us.org");
+  await page.click("id=password");
+  await page.locator("id=password").fill("carbon");
+  await page.pause();
+  await page.getByRole("button", { name: "Sign In" }).click();
 });
